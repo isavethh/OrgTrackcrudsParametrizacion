69142729d0d3f6302d6b1c658960d29eeaa871e4
isavethh <isavethg@gmail.com>
2025-11-12 14:58:39 -0400

    mig1

---
+++ database/migrations/2025_11_12_155247_create_cliente_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('usuario_id')->unique()->constrained('usuario')->onDelete('cascade');
+            $table->string('telefono', 20)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cliente');
+    }
+};
