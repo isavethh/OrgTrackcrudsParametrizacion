ad8b66461bfb0a9065ca0070d7c7f6009750ec7f
Franco Torrez <franco.torrez.alv@gmail.com>
2025-12-13 05:17:17 -0400

    fads

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,7 +23,11 @@ return Application::configure(basePath: dirname(__DIR__))
         */
         $middleware->trustProxies(
             at: '*',
-            headers: Request::HEADER_X_FORWARDED_ALL
+            headers:
+                Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_PROTO |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT
         );
 
         /*
